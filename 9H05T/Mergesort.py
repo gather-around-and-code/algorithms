31053980a33899d9f8def5f517054699c970a173
Dongsoon Shin <dsshin@ls-electric.com>
2022-12-07 10:20:23 +0900

    add: mergesort implment

---
+++ 9H05T/Mergesort.py
@@ -0,0 +1,14 @@
+import sys
+
+
+def merge(A, p, q, r):
+    return
+
+if __name__ == '__main__':
+    input = sys.stdin.readline
+    A = list(map(int, input().split()))
+    p = 0
+    q = len(A) // 2 + 1
+    r = list()
+
+    merge(A , p, q, r)
