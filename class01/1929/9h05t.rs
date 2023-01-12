761a444878bb1bd1482d2a5a3ce77052663f9be0
Dongsoon Shin <dsshin@ls-electric.com>
2023-01-12 11:26:05 +0900

    1929. 소수 구하기 #12, not complete, 20230112

--- class01/1929/9h05t.rs
+++ class01/1929/9h05t.rs
@@ -1,21 +1,47 @@
 use std::io;
 
 fn main() {
-  let mut line = String::new();
-  io::stdin().read_line(&mut line).expect("");
+  let limit = 100;
+  // let mut line = String::new();
+  // io::stdin().read_line(&mut line).expect("");
 
-  let inputs: Vec<i32> = line.split_whitespace()
-  .map(|f| f.parse().expect(""))
-  .collect();
-
-  let mut m = inputs[0];
-  let mut n = inputs[1];
-
-  let mut arr:[i32;10000000];
-  let stop:i32 = arr.len() as i32;
-
-  // how can initilize array in Rust?
-  for i in 0..stop {
-    arr[i] = i;
+  let mut is_prime = vec![true; limit + 1];
+  is_prime[0] = false;
+  if limit >= 1 {
+    is_prime[1] = false 
   }
+
+  for num in 2..limit+1 {
+    if is_prime[num] {
+      let mut multiple = num * num;
+      println!("{}", multiple);
+      while multiple <= limit {
+        is_prime[multiple] = false;
+        multiple = num;
+      }
+    }
+  }
+
+  let v:Vec<usize> = is_prime.iter().enumerate()
+    .filter_map(|(pr, &is_pr)| if is_pr { Some(pr) } else {None})
+    .collect();
+
+  for i in 5..10 {
+    if is_prime[i] {
+      println!("{}", v[i]);
+    }
+  }
+
+  // let inputs: Vec<usize> = line.split_whitespace()
+  // .map(|f| f.parse().expect(""))
+  // .collect();
+
+  // let mut m:usize = inputs[0];
+  // let mut n:usize = inputs[1];
+
+  // for index in m..n {
+  //   if is_prime[index] {
+  //     println!("{}", index);
+  //   }
+  // }
 }
