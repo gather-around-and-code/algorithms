c309cd52d0f9bed9de5da049a2d6821615381895
Dongsoon Shin <dsshin@ls-electric.com>
2023-01-25 14:14:03 +0900

    update: 20230125 timeout fix

--- class01/1929/9h05t.rs
+++ class01/1929/9h05t.rs
@@ -1,4 +1,5 @@
 use std::{io, usize};
+use std::fmt::Write;
 fn main() {
   let max = 1000001;
   let mut buf = String::new();
@@ -6,23 +7,33 @@ fn main() {
   .expect("");
 
   let s: Vec<usize> = buf.split_ascii_whitespace().into_iter().map(|x| x.parse::<usize>().unwrap_or(0)).collect();
-  let mut primes: Vec<bool> = Vec::new();
-  for _ in 0..max {
-    primes.push(true);
-  }
-  
-  let m = (max as f64).sqrt() as usize;
-  for i in 2..m+1 {
-    if primes[i] {
-      for j in (i+i..max).step_by(i) {
-        primes[j] = false;
+  let mut is_prime = vec![true; s[1] as usize + 1];
+  is_prime[0] = false;
+  is_prime[1] = false;
+
+  let mut c = 2 as usize;
+  let m = (s[1] as f64).sqrt() as usize + 1;
+
+  while c <= m as usize {
+      if is_prime[c] {
+        let mut mul = 2;
+        let mut i = c * mul;
+
+        while i <= s[1] as usize {
+          is_prime[i] = false;
+          mul += 1;
+          i = c * mul;
+        }
       }
+      c += 1
+  }
+
+  let mut res = String::new();
+  for i in s[0]..=s[1] {
+    if is_prime[i] {
+      writeln!(res, "{}", i).unwrap();
     }
   }
 
-  for i in s[0]..=s[1] {
-    if primes[i] {
-      println!("{}", i.to_string());
-    }
-  }
+  println!("{}", res);
 }
