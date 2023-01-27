fba4c97c97773e7fdd6cf7284b8c57bbaf02faad
Dongsoon Shin <dsshin@ls-electric.com>
2023-01-27 15:37:07 +0900

    new: 2023-01-27 4949 rust complete

--- class02/4949/9h05t.rs
+++ class02/4949/9h05t.rs
@@ -3,31 +3,47 @@ use std::{io};
 fn main() {
   loop {
     let mut buf = String::new();
-    io::stdin().read_line(&mut buf).expect("");
-    let s = buf.trim();
-    
     let mut char_stack:Vec<char> = Vec::new();
 
-    for i in s.chars() {
-      if i == '.' {
-        return;
-      }
+    io::stdin().read_line(&mut buf).expect("");
+    let s = buf;
 
+    if s.chars().nth(0) == Some('.') {
+      return;
+    }
+
+    for i in s.chars() {
       if i == '(' || i == '[' {
         char_stack.push(i);
+        continue;
       }
 
       if i == ')' {
-        let c = char_stack.pop().unwrap();
-        if c == '(' {
-          continue;
+        if (!char_stack.is_empty()) && (char_stack[char_stack.len() - 1] == '(') {
+          char_stack.pop();
+        }
+        else { 
+          println!("no"); 
+          break;
         }
       }
+
       if i == ']' {
-        let c = char_stack.pop().unwrap();
-        if c == '[' {
-          continue;
+        if (!char_stack.is_empty()) && (char_stack[char_stack.len() - 1] == '[') {
+          char_stack.pop();
         }
+        else {
+            println!("no");
+            break;
+        }
+      }
+
+
+      if char_stack.is_empty() && i == '.' {
+        println!("yes");
+      }
+      else if !char_stack.is_empty() && i == '.' {
+        println!("no");
       }
     }
   }
